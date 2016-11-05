b86d1e09f5e689a2e201932289ff3590c85e9317
alexKononok <alex.kononok@gmail.com>
2016-11-05 11:04:33 +0300

    Added prototypes of functions

--- hello_timer.c
+++ hello_timer.c
@@ -3,4 +3,17 @@
 #include <linux/device.h>
 #include <linux/timer.h>
 #include <linux/fs.h>
+#define DELAY 3
+#define TEXT "Hello, world!"
+
+static unsigned long times = 0;
+static unsigned char timer_exists = 0;
+
+static int timer_init(void);
+static void timer_exit(void);
+
+static ssize_t times_show(struct kobject *, struct kobj_attribute *, char *);
+static ssize_t times_store(struct kobject *, struct kobj_attribute *, const char *, size_t);
+
+static void repeat(unsigned long);
 
